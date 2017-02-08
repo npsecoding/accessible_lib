378e4f35eca63fc1b0e788c4c88fc3669fc43ac0
Nancy Pang <npang@mozilla.com>
2017-02-08 12:54:03 -0800

    Changed getting test window and added comments

--- accessible_lib/utils/win.py
+++ accessible_lib/utils/win.py
@@ -1,6 +1,6 @@
 """Utility for Windows Platform"""
 
-from ctypes import oledll, byref, POINTER
+from ctypes import windll, oledll, byref, POINTER
 from ctypes.wintypes import c_char_p, c_long
 from comtypes.automation import VARIANT
 from ..scripts.constants import *
@@ -8,7 +8,9 @@ from ..scripts.debug import *
 from ..utils.IUtil import IUtil
 
 class WinUtil(IUtil):
-    """Utility definition for Windows Platform"""
+    """
+    Utility definition for Windows Platform
+    """
 
     def __init__(self):
         super(WinUtil, self).__init__()
@@ -18,10 +20,13 @@ class WinUtil(IUtil):
         self.get_root_accessible()
 
     def _accessible_object_from_window(self, hwnd):
-        """Get the accessible object for window"""
+        """
+        Get the accessible object for window
+        """
         acc_ptr = POINTER(IAccessible_t)()
         res = oledll.oleacc.AccessibleObjectFromWindow(
             hwnd, OBJID_WINDOW, byref(IID_IAccessible), byref(acc_ptr))
+
         if res == S_OK:
             acc_ptr.children = self._accessible_children(acc_ptr)
             return acc_ptr
@@ -29,7 +34,9 @@ class WinUtil(IUtil):
             raise ValueError("Can't get accessible from window")
 
     def _accessible_children(self, accptr):
-        """Get the children of an accessible object"""
+        """
+        Get the children of an accessible object
+        """
         ichild_start = 0
         cc_children = accptr.accChildCount
         pc_obtained = c_long()
@@ -38,48 +45,76 @@ class WinUtil(IUtil):
         res = oledll.oleacc.AccessibleChildren(
             accptr, ichild_start, cc_children, byref(rgvar_children), byref(pc_obtained))
 
-        # Get IAccessible objects
-        acc_objs = []
-        for child in rgvar_children:
-            if child.vt == VT_DISPATCH:
-                acc = child.value.QueryInterface(IAccessible_t)
-                acc_objs.append(acc)
-            elif child.vt == VT_I4:
-                self._wrap_simple_element(accptr, child.value)
-        return acc_objs
+        if res == S_OK:
+            acc_objs = []
+            for child in rgvar_children:
+                # Child is IAccessible
+                if child.vt == VT_DISPATCH:
+                    acc = child.value.QueryInterface(IAccessible_t)
+                    acc_objs.append(acc)
+                # Child is Simple Element
+                elif child.vt == VT_I4:
+                    self._wrap_simple_element(accptr, child.value)
+            return acc_objs
+        else:
+            raise ValueError("Can't get accessible children")
 
     def _wrap_simple_element(self, accptr, childid):
+        """
+        Associate simple element and parent accessible object
+        """
         if accptr not in self._simple_elements:
             self._simple_elements[accptr] = [childid]
         else:
             self._simple_elements[accptr].append(childid)
 
     def _traverse(self, node, acc_id, visited):
+        """
+        Traverse through accessible tree looking for node with the given ID
+        """
         if node.accName(CHILDID_SELF) == acc_id:
             self._target = node
             return
 
         print_accessible(node)
 
+        # Traverse through simple elements of accessible object
         if node in self._simple_elements:
             for childid in self._simple_elements[node]:
                 print_simple(node, childid)
+
+        # Traverse through each accessible object's children
         for child in self._accessible_children(node):
             if child not in visited:
                 visited.add(node)
                 self._traverse(child, acc_id, visited)
 
-    def get_root_accessible(self):
-        """Get the window for the browser"""
+    def _get_test_window(self):
+         # Get the window for browser
         test_class = c_char_p("MozillaWindowClass")
-        """TODO FIX FINDING WINDOW MULTIPLE CLASSES"""
-        # test_window = windll.user32.FindWindowA(test_class, None)
-        test_window = 0xA0450
+        current_hwnd = windll.user32.FindWindowA(test_class, None)
+        name = self._accessible_object_from_window(current_hwnd).accName(CHILDID_SELF)
+
+        # Iterate through windows
+        while name is None or 'Nightly' not in name:
+            current_hwnd = windll.user32.FindWindowExA(None, current_hwnd, test_class, None)
+            name = self._accessible_object_from_window(current_hwnd).accName(CHILDID_SELF)
+
+        return current_hwnd
+
+    def get_root_accessible(self):
+        """
+        Set root accessible object to test window
+        """
+        test_window = self._get_test_window()
         print 'Test Window: %d' %test_window
         self._root = self._accessible_object_from_window(test_window)
         return self._root
 
     def get_target_accessible(self, acc_id):
+        """
+        Retrieve the accessible object for the given ID
+        """
         visited = set()
         visited.add(self._root)
         self._traverse(self._root, acc_id, visited)
