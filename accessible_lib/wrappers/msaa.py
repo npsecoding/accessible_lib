3f05863b83ecf0f888326113cb899146d44f6316
Nancy Pang <npang@mozilla.com>
2017-02-10 14:52:52 -0800

    Serialize nested children

--- accessible_lib/wrappers/msaa.py
+++ accessible_lib/wrappers/msaa.py
@@ -8,43 +8,52 @@ class MSAA(NsIAccessible):
     def __init__(self, acc_id):
         super(MSAA, self).__init__(acc_id)
         self.parent = self.get_acc_parent()
-        self.children = self.get_acc_children()
-    def get_acc_children(self):
+        child_tree = {'children': ""}
+        self.children = self.get_acc_children(self._target, child_tree, True)
+
+    def get_acc_children(self, acc_ptr, tree, first):
         """Get child accessible"""
-        parent = self._target
+        # First is used to determine if a children field should wrap list
+        parent = acc_ptr
         children_ptr = self._util._accessible_children(parent)
-        children = map(pointer_wrap_child, children_ptr)
-        return children
+        if first:
+            tree = map(pointer_wrap, children_ptr)
+        else:
+            tree['children'] = map(pointer_wrap, children_ptr)
+
+        for index, child_ptr in enumerate(children_ptr):
+            if first:
+                self.get_acc_children(child_ptr, tree[index], False)
+            else:
+                self.get_acc_children(child_ptr, tree['children'][index], False)
+
+        return tree
+
     def get_acc_focus(self):
         """Get focus"""
         """TODO"""
+
     def get_acc_name(self):
         """Get computed name"""
         return self._target.accName(CHILDID_SELF)
+
     def get_acc_parent(self):
         """Get parent accessible"""
-        return pointer_wrap_parent(self._target.accParent)
+        return pointer_wrap(self._target.accParent)
+
     def get_acc_role(self):
         """Get computed role"""
         return self._target.accRole(CHILDID_SELF)
+
     def get_acc_state(self):
         """Get computed state"""
         return self._target.accState(CHILDID_SELF)
+
     def get_acc_value(self):
         """Get computed value"""
         return self._target.accValue(CHILDID_SELF)
 
-def pointer_wrap_parent(acc_ptr):
-    """Convert pointer to object for serialization"""
-    return {
-        # Not returning parent field in parent pointer
-        'name': acc_ptr.accName(CHILDID_SELF),
-        'role': acc_ptr.accRole(CHILDID_SELF),
-        'state': acc_ptr.accState(CHILDID_SELF),
-        'value': acc_ptr.accValue(CHILDID_SELF)
-    }
-
-def pointer_wrap_child(acc_ptr):
+def pointer_wrap(acc_ptr):
     """Convert pointer to object for serialization"""
     print acc_ptr
     return {
