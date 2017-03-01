ded172f151c09e0c4252b037f66ab546ee26a5f1
Nancy Pang <npang@mozilla.com>
2017-03-01 11:54:26 -0800

    Change test format

--- tests/test_msaa_role.py
+++ tests/test_msaa_role.py
@@ -13,21 +13,25 @@ client.start_session()
 test_html = localhost + filePort + "/"+ 'test_msaa_role.html'
 client.navigate(test_html)
 
-event_params = urllib.urlencode({'id': 'MSAA Link', 'type' : 'EVENT_OBJECT_STATECHANGE'})
+event_params = urllib.urlencode({'id': 'MSAA Checkbox', 'type' : 'EVENT_OBJECT_STATECHANGE'})
 event_endpoint = localhost + serverPort + "/event?%s"
 event_register_params = urllib.urlencode({'type' : 'EVENT_OBJECT_STATECHANGE'})
 event_register_endpoint = localhost + serverPort + "/event_register?%s"
-accessible_params = urllib.urlencode({'id': 'MSAA Link', 'depth': -1})
+accessible_params = urllib.urlencode({'id': 'MSAA Checkbox', 'depth': -1})
 accessible_endpoint = localhost + serverPort + "/accessible?%s"
 
 response = json.load(urllib.urlopen(accessible_endpoint % accessible_params))
-assert response['role'] == 'link'
+assert response['role'] == 'check box'
 pprint(response)
 
 response = json.load(urllib.urlopen(event_register_endpoint % event_register_params))
-button = client.find_element(By.TAG_NAME, "a")
-button.click()
+print response
+
+checkbox = client.find_element(By.CSS_SELECTOR, "input[type='checkbox']")
+checkbox.click()
+
 response = json.load(urllib.urlopen(event_endpoint % event_params))
+print response
 
 
 
