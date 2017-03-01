83a72d21469744099b173cb2cdc80479b15b0a94
Nancy Pang <npang@mozilla.com>
2017-02-28 17:10:26 -0800

    Remove debug statements for accessible traversal

--- accessible_lib/utils/win.py
+++ accessible_lib/utils/win.py
@@ -76,12 +76,12 @@ class WinUtil(IUtil):
             self._target = node
             return
 
-        print_accessible(node)
+        # print_accessible(node)
 
         # Traverse through simple elements of accessible object
-        if node in self._simple_elements:
-            for childid in self._simple_elements[node]:
-                print_simple(node, childid)
+        # if node in self._simple_elements:
+        #     for childid in self._simple_elements[node]:
+        #         print_simple(node, childid)
 
         # Traverse through each accessible object's children
         for child in self._accessible_children(node):
